97fac106c94453e30638c3e58fd08460e151de97
Avrhambi <avrham9111@gmail.com>
2026-02-18 12:20:09 +0200

    Remove specific path from requirements installation

--- config/install_dependencies.bat
+++ config/install_dependencies.bat
@@ -1,6 +1,5 @@
 @echo off
 echo Installing Python packages...
 python -m pip install --upgrade pip
-python -m pip install -r "C:\Users\shimo\JINI\back\JINI\config\requirements.txt"
 echo All dependencies installed successfully!
 pause
